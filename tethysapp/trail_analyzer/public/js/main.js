f4c042d2fef03969878b44ae98b651b390ddaf8d
ezrajrice <ezra.j.rice@gmail.com>
2017-03-28 08:41:46 -0600

    js demo from class

--- tethysapp/trail_analyzer/public/js/main.js
+++ tethysapp/trail_analyzer/public/js/main.js
@@ -1,4 +1,24 @@
 "use strict";
 $(document).ready(function() {
+    var service_url = '';
+    var layers = [
+        new ol.layer.Tile({
+            source: new ol.source.OSM()
+        }),
+        new ol.layer.Tile({
+            extent: [-13884991, 2870341, -7455066, 6338219],
+            source: new ol.source.TileArcGISRest({
+                url: service_url
+            })
+        })
+    ];
 
+    var map = new ol.Map({
+        layers: layers,
+        target: 'map',
+        view: new ol.View({
+            center: [-10997148, 4569099],
+            zoom: 4
+        })
+    });
 }
