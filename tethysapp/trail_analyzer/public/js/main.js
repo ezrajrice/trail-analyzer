cccfe5f1299e6d33cd2b61711a0ce0cc787eee79
ezrajrice <ezra.j.rice@gmail.com>
2017-04-10 16:34:52 -0600

    trails img layer loading

--- tethysapp/trail_analyzer/public/js/main.js
+++ tethysapp/trail_analyzer/public/js/main.js
@@ -1,24 +1,24 @@
 "use strict";
 $(document).ready(function() {
-    var service_url = 'http://geoserver.byu.edu:6080/arcgis/rest/services/dan_ames/roads/MapServer';
-    var layers = [
-        new ol.layer.Tile({
-            source: new ol.source.OSM()
-        }),
-        new ol.layer.Tile({
-            extent: [-13884991, 2870341, -7455066, 6338219],
-            source: new ol.source.TileArcGISRest({
-                url: service_url
-            })
-        })
-    ];
 
-    var map = new ol.Map({
-        layers: layers,
-        target: 'map',
-        view: new ol.View({
-            center: [-10997148, 4569099],
-            zoom: 4
-        })
-    });
+    //This "require" function is used by the ESRI/DOJO system. It loads modules and then runs the "function" code
+ 	//So your custom code for the most part goes in the function block below.
+ 	require([
+ 	"esri/layers/ArcGISDynamicMapServiceLayer",
+ 	"esri/layers/ArcGISTiledMapServiceLayer",
+ 	"esri/map",
+ 	"dojo/domReady!"
+ 	],
+
+ 	function(ArcGISDynamicMapServiceLayer, ArcGISTiledMapServiceLayer, Map, parser){
+        //This is the main function that will run once the page has fully loaded
+        //Create a map object with a default "streets" basemap:
+        var map = new Map("mapDiv", {basemap: "streets", center: [-111.6, 40.23], zoom:8});
+
+        var trails_url = "http://geoserver.byu.edu:6080/arcgis/rest/services/Ellie_Ott/trails/MapServer";
+
+        //Create a layer object that pulls your web mapping service from the geoserver.byu.edu server:
+        var trails_layer = new ArcGISDynamicMapServiceLayer(trails_url);
+        map.addLayer(trails_layer);
+ 	});
 })
