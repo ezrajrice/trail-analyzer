fe1328aa6f9e16750f37e868ea9c6053d5404ae6
ezrajrice <ezra.j.rice@gmail.com>
2017-04-17 16:53:54 -0600

    Waiting for geoprocessing to be up and working. Fixed display issues. Added dynamic plot.

--- tethysapp/trail_analyzer/controllers.py
+++ tethysapp/trail_analyzer/controllers.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from tethys_sdk.gizmos import LinePlot
 
 
 @login_required()
@@ -7,6 +8,34 @@ def home(request):
     """
     Controller for the app home page.
     """
-    context = {}
+    # Create Highcharts plot here
+    elevation_plot = LinePlot(
+        height='500px',
+        width='500px',
+        engine='highcharts',
+        title='Trail Plot',
+        subtitle='',
+        spline=True,
+        x_axis_title='Distance',
+        x_axis_units='miles',
+        y_axis_title='Elevation',
+        y_axis_units='ft',
+        series=[
+            {
+                'name': 'Air Temp',
+                'color': '#0066ff',
+                'marker': {'enabled': False},
+                'data': [
+                    [0, 5], [10, -70],
+                    [20, -86.5], [30, -66.5],
+                    [40, -32.1],
+                    [50, -12.5], [60, -47.7],
+                    [70, -85.7], [80, -106.5]
+                ]
+            }
+        ]
+    )
+
+    context = {'elevation_plot': elevation_plot}
 
     return render(request, 'trail_analyzer/home.html', context)
--- tethysapp/trail_analyzer/public/js/main.js
+++ tethysapp/trail_analyzer/public/js/main.js
@@ -15,16 +15,22 @@ $(document).ready(function() {
 
  	function(ArcGISTiledMapServiceLayer, FeatureLayer, Map, Query, Circle, esriLang, parser){
         //This is the main function that will run once the page has fully loaded
+
+        // bind button functionality
+        $('#show-plot').on('click', showPlot);
+        $('#close-plot').on('click', hidePlot);
+
         //Create a map object with a default "streets" basemap:
         var map = new Map("mapDiv", {basemap: "streets", center: [-111.6, 40.23], zoom: 10, slider: false});
 
         var trails_url = "http://geoserver.byu.edu:6080/arcgis/rest/services/Ellie_Ott/trails/MapServer/0";
-        var geoprocessing_url = "http://geoserver.byu.edu:6080/arcgis/rest/services/Ellie_Ott/hiking/GPServer/hiking/execute?ID_input=[[TRAIL_ID]]&env%3AoutSR=&env%3AprocessSR=&returnZ=true&returnM=true&f=json";
+        var geoprocessing_url = "http://geoserver.byu.edu:6080/arcgis/rest/services/Ellie_Ott/hiking/GPServer/hiking/execute?" +
+            "ID_input=[[TRAIL_ID]]&env%3AoutSR=&env%3AprocessSR=&returnZ=true&returnM=true&f=json";
 
         //Create a layer object that pulls your web mapping service from the geoserver.byu.edu server:
         var trails_layer = new FeatureLayer(trails_url, {
             mode: FeatureLayer.MODE_SNAPSHOT,
-            outFields: ["TrailID"]
+            outFields: ["TrailID", "SHAPE_Leng"]
         });
 
         map.addLayer(trails_layer);
@@ -33,16 +39,12 @@ $(document).ready(function() {
             map.graphics.enableMouseEvents();
         });
 
-//        trails_layer.on("click", function(evt) {
-//            var t = "${TrailID}";
-//            var content = esriLang.substitute(evt.graphic.attributes,t);
-//            console.log(content, "event-fired");
-//        });
-
         var circle;
 
         //when the map is clicked create a buffer around the click point of the specified distance.
         map.on("click", function(evt){
+            // Disable plot button
+            $('#show-plot').prop('disabled', true);
             circle = new Circle({
                 center: evt.mapPoint,
                 geodesic: true,
@@ -56,25 +58,62 @@ $(document).ready(function() {
             query.geometry = circle.getExtent();
             //use a fast bounding box query. will only go to the server if bounding box is outside of the visible map
             var selectedTrail = trails_layer.queryFeatures(query, selectInBuffer).results[0].features[0];
+            var trail_length = selectedTrail.attributes.SHAPE_Leng;
             console.log(selectedTrail);
             // Fire AJAX call to server
+            var ajax_url = geoprocessing_url.replace('[[TRAIL_ID]]', selectedTrail.attributes.TrailID);
+            console.log(ajax_url)
+            $.ajax({
+                url : ajax_url,
+                type : 'GET',
+                dataType:'json',
+                success : function(data) {
+                    console.log(data);
+                    // Set data to be data of plot
+                    var test_data = [120, 140, 128, 147, 148, 153, 160];
+                    var plot_data = [];
+                    for (var i=0; i<data.length; i++) {
+                        var x = (trail_length / data.length) * i;
+                        var y = data[i];
+                        plot_data.push([x,y]);
+                    }
+                    updatePlot(test_data);
+                },
+                error : function(request, error)
+                {
+                    console.log("Request: "+JSON.stringify(request));
+                }
+            });
         });
 
         function selectInBuffer(response){
             var feature;
             var features = response.features;
-            // Get the last feature returned so only one gets sent
+            // Get the last feature returned so only one gets sent to geoprocess
             feature = features[features.length - 1];
             var TrailID = feature.attributes.TrailID;
             var query = new Query();
             query = TrailID;
+
+            // todo Change the selected features display
+
             return TrailID;
-            //use a fast objectIds selection query (should not need to go to the server)
-//            trails_layer.selectFeatures(query, FeatureLayer.SELECTION_NEW, function(results){
-//                var t = "${TrailID}";
-//                var content = esriLang.substitute(evt.graphic.attributes, t);
-//                console.log(content, "event-fired");
-//            });
+        }
+
+        function updatePlot(data) {
+            // this gets called when data is returned
+            Highcharts.charts[1].series[0].setData(data);
+
+            // Make plot button clickable
+            $('#show-plot').prop('disabled', false);
+        }
+
+        function showPlot() {
+            $('#plot-container').addClass('show');
+        }
+
+        function hidePlot() {
+            $('#plot-container').removeClass('show');
         }
  	});
 })
