7c4cf1e639e8c41e194d2e0fb5363eb0c2721ab8
ezrajrice <ezra.j.rice@gmail.com>
2017-04-18 22:11:21 -0600

    Updated code to get the data from the process, but somehow our trails mapservice (layer) got deleted. Can't test or show how the app works...

--- tethysapp/trail_analyzer/public/js/main.js
+++ tethysapp/trail_analyzer/public/js/main.js
@@ -24,8 +24,9 @@ $(document).ready(function() {
         var map = new Map("mapDiv", {basemap: "streets", center: [-111.6, 40.23], zoom: 10, slider: false});
 
         var trails_url = "http://geoserver.byu.edu:6080/arcgis/rest/services/Ellie_Ott/trails/MapServer/0";
-        var geoprocessing_url = "http://geoserver.byu.edu:6080/arcgis/rest/services/Ellie_Ott/hiking/GPServer/hiking/execute?" +
-            "ID_input=[[TRAIL_ID]]&env%3AoutSR=&env%3AprocessSR=&returnZ=true&returnM=true&f=json";
+        var geoprocessing_url = "http://geoserver.byu.edu:6080/arcgis/rest/services/Ellise_Hiking/Ellie_hiking/GPServer/hiking/submitJob?" +
+            "ID_input=[[TRAIL_ID]]&env%3AoutSR=&env%3AprocessSR=&returnZ=true&returnM=false&f=pjson";
+        var results_url = "http://geoserver.byu.edu:6080/arcgis/rest/services/Ellise_Hiking/Ellie_hiking/GPServer/hiking/jobs/[[JOB_ID]]/results/points?f=pjson";
 
         //Create a layer object that pulls your web mapping service from the geoserver.byu.edu server:
         var trails_layer = new FeatureLayer(trails_url, {
@@ -61,23 +62,16 @@ $(document).ready(function() {
             var trail_length = selectedTrail.attributes.SHAPE_Leng;
             console.log(selectedTrail);
             // Fire AJAX call to server
-            var ajax_url = geoprocessing_url.replace('[[TRAIL_ID]]', selectedTrail.attributes.TrailID);
-            console.log(ajax_url)
+            var geo_url = geoprocessing_url.replace('[[TRAIL_ID]]', selectedTrail.attributes.TrailID);
+            console.log(geo_url)
             $.ajax({
                 url : ajax_url,
                 type : 'GET',
                 dataType:'json',
                 success : function(data) {
                     console.log(data);
-                    // Set data to be data of plot
-                    var test_data = [120, 140, 128, 147, 148, 153, 160];
-                    var plot_data = [];
-                    for (var i=0; i<data.length; i++) {
-                        var x = (trail_length / data.length) * i;
-                        var y = data[i];
-                        plot_data.push([x,y]);
-                    }
-                    updatePlot(test_data);
+                    var job_id = data.jobId;
+                    getTrailData(job_id, trail_length);
                 },
                 error : function(request, error)
                 {
@@ -85,6 +79,30 @@ $(document).ready(function() {
                 }
             });
         });
+
+        function getTrailData(job_id, trail_length) {
+            res_url = results_url.replace('[[JOB_ID]]', job_id);
+            $.ajax({
+                url : res_url,
+                type : 'GET',
+                dataType:'json',
+                success : function(data) {
+                    console.log(data);
+                    // Set data to be data of plot
+                    var plot_data = [];
+                    for (var i=0; i<data.features.length; i++) {
+                        var x = (trail_length / data.length) * i;
+                        var y = data.features[i].grid_code;
+                        plot_data.push([x,y]);
+                    }
+                    updatePlot(plot_data);
+                },
+                error : function(request, error)
+                {
+                    console.log("Request: "+JSON.stringify(request));
+                }
+            });
+        }
 
         function selectInBuffer(response){
             var feature;
